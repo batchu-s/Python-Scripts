0308352fc22b852985ff7d3cec501afa64cc56e6
batchu-s <36170966+batchu-s@users.noreply.github.com>
2018-02-15 08:35:45 -0600

    Add files via upload

---
+++ Program1.py
@@ -0,0 +1,24 @@
+__author__ = "Sumanth Kumar Batchu"
+
+import boto3
+
+regions = ['us-east-2', 'us-east-1', 'us-west-1', 'us-west-2', 'ap-south-1', 'ap-northeast-2',
+           'ap-southeast-1', 'ap-southeast-2', 'ap-northeast-1', 'ca-central-1',
+           'eu-central-1', 'eu-west-1', 'eu-west-2', 'eu-west-3', 'sa-east-1']
+
+
+def print_instance_details(response):
+    for item in response["Reservations"]:
+        for instance in item["Instances"]:
+            print('{:50s}'.format(instance["PublicDnsName"]), end=': ')
+            print('{:20s}'.format(instance["PublicIpAddress"]), end='   ')
+            print(instance["Placement"]["AvailabilityZone"])
+            print("---------------------------------------------------------------------------------------")
+
+        
+
+for region in regions:
+    ec2_conn = boto3.client('ec2', region_name=region)
+    print_instance_details(ec2_conn.describe_instances())
+
+
---
+++ Program7.py
@@ -0,0 +1,8 @@
+__author__ = "Sumanth Kumar Batchu"
+
+import boto3
+
+ec2_conn = boto3.client('ec2')
+response = ec2_conn.create_image(InstanceId='i-01d99b0bea4f7634a', Name='ami-newsnap')
+
+print(response['ImageId'])
