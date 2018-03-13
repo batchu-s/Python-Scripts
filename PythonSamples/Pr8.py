b03ab2885527efe269d472b07744e57556bd7553
root <root@ip-172-31-37-209.ec2.internal>
2018-03-13 01:59:42 +0000

    Largest using conditional expression

---
+++ PythonSamples/Pr8.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python36
+
+print("Enter two numbers: ")
+a=int(input("Enter the value of a: "))
+b=int(input("Enter the value of b: "))
+l= a if a>b else b
+print(l," is the largest")
+ 
