15d11343d72f67893fe51385824a3b7c7cd63f87
Sumanth Kumar Batchu <sumanthkumar060895@outlook.com>
2024-12-10 12:32:59 -0600

    Update Program8.py

--- Program8.py
+++ Program8.py
@@ -4,6 +4,6 @@ import boto3
 
 s3_conn = boto3.client('s3')
 response = s3_conn.create_bucket(Bucket='techuser')
-response1 = s3_conn.upload_file('C:\\Users\\suman\\MS\\Consultancy\\Python\\test.txt', 'techuser', 'test.txt')
+response1 = s3_conn.upload_file('C:\\Users\\suman\\MS\\Python\\test.txt', 'techuser', 'test.txt')
 
 print(response)
